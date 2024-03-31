57556081c50c68111dc7591c198ff4a82a8197a6
Kojo Patrick <patrickasare84@gmail.com>
2024-03-31 16:58:49 +0000

    Added todo comments for future improvements: increasing game speed, displaying game level, designing intro screen, and storing high score

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,11 @@ import Game from '@/components/Game';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import React from 'react';
 
+//TODO: 1. Increase game speed upon certain score
+//TODO: 2. Toast game level as speed game speed increase
+//TODO: 3. Design a welcome game intro screen
+//TODO: 4. Store HighScore
+
 const App = () => (
 	<GestureHandlerRootView>
 		<Game />
