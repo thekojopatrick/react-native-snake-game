1b294057bcd1915beda848b5283d651dd3f19465
Kojo Patrick <patrickasare84@gmail.com>
2024-05-01 17:07:08 +0000

    [wip]

---
+++ components/CustomGameHeader.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native';
+
+import React from 'react';
+
+const CustomGameHeader = () => {
+	return (
+		<View>
+			<Text>CustomGameHeader</Text>
+		</View>
+	);
+};
+
+export default CustomGameHeader;
